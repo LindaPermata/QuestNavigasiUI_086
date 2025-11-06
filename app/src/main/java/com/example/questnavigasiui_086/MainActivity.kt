fb7f4d012bac81625695e2f3672cfbad68504bf5
LindaPermata <permatalinda531@gmail.com>
2025-11-06 14:13:01 +0700

    Memperbaiki eror

--- app/src/main/java/com/example/questnavigasiui_086/MainActivity.kt
+++ app/src/main/java/com/example/questnavigasiui_086/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestNavigasiUI_086Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
