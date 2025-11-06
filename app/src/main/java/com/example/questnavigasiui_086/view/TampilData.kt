a117e2c8b10f86a2373decad997b959d6c272bb3
LindaPermata <permatalinda531@gmail.com>
2025-11-06 14:07:05 +0700

    Merapikan code di TampilData dan menambahkan import di Navigasi

--- app/src/main/java/com/example/questnavigasiui_086/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_086/view/TampilData.kt
@@ -25,7 +25,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.questnavigasiui_086.R
 
-@OptIn(ExperimentalMaterial3Api::class))
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TampilData(
     onBackBtnClick:()->Unit
