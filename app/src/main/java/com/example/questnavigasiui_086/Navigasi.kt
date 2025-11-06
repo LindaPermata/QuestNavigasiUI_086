ea29d015716f524fe83d912e725004857ec9ebad
LindaPermata <permatalinda531@gmail.com>
2025-11-06 12:11:14 +0700

    Membuat function data app

--- app/src/main/java/com/example/questnavigasiui_086/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasiui_086/Navigasi.kt
@@ -1,7 +1,30 @@
 package com.example.questnavigasiui_086
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+
 enum class Navigasi {
     Formulirku,
     Detail
 }
 
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+){
+    Scaffold { isisRuang->
+        NavHost(
+            NavController = navController,
+            startDestination = Navigasi.Formulirku.name,
+
+
+        )
+
+    }
+
+}
