4c9b792cc91851c963907cfaf6d29210ffdc5393
LindaPermata <permatalinda531@gmail.com>
2025-11-06 13:43:10 +0700

    Menambahkan divider, textfield alamat, dan button submit

--- app/src/main/java/com/example/questnavigasiui_086/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_086/view/Formulir.kt
@@ -2,9 +2,12 @@
 
 package com.example.questnavigasiui_086.view
 
+import android.R.attr.thickness
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.DividerDefaults.Thickness
@@ -69,6 +72,24 @@ fun FormIsian(
                         }
                     }
                 }
+                HorizontalDivider(modifier = Modifier
+                    .padding(20.dp)
+                    .width(250.dp),
+                    thickness = 1.dp,
+                    color = Color.Red
+                )
+                OutlinedTextField(
+                    value = "",
+                    singleLine = true,
+                    modifier = Modifier
+                        .width(250.dp),
+                    label = { Text(text = "Alamat") },
+                    onValueChange = {},
+                )
+                Spacer(modifier = Modifier.height(30.dp))
+                Button(
+
+                )
             }
 
         }
